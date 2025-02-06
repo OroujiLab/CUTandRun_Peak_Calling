68dc9f9c6076d4677694bbcc174e715aaeb08c5b
Aminnn <45661298+aminthedream@users.noreply.github.com>
2025-02-05 22:49:58 -0500

    Update 02_MACS2.sh
    organized macs2

--- peak_calling/hg38/02_MACS2.sh
+++ peak_calling/hg38/02_MACS2.sh
@@ -1,29 +1,18 @@
 #!/bin/bash
-#SBATCH -J bench_macs2_4D
-#SBATCH -N 1
-#SBATCH -c 1
-#SBATCH -p himem
-#SBATCH --time=1-00:00:00
-#SBATCH --mem=50G
-#SBATCH -o benchmarking_macs2.log
-#SBATCH -e benchmarking_macs2.err
 
 
-# Set the base directories
+
 BAM_DIR="/cluster/projects/epigenomics/Aminnn/CNR/EpigenomeLab/EPI_P003_CNR_MM10_07172022/Four_DN/batch_2/sorted_bams/filese_renamed_adjusted/HFFc6"
 OUTPUT_DIR="/cluster/projects/epigenomics/Aminnn/CNR/EpigenomeLab/EPI_P003_CNR_MM10_07172022/Four_DN/batch_2/sorted_bams/filese_renamed_adjusted/results_2/MACS2/HFFc6"
 
-# Create output directory
 mkdir -p "$OUTPUT_DIR"
 
-# Function to find matching IgG control
 find_igg_control() {
     local basename=$1
     local celltype=$(echo $basename | cut -d'_' -f3-)
     celltype=${celltype%_R*}  # Remove replicate information
     local replicate=$(echo $basename | grep -oP '_R\d+' | sed 's/^_//')
     
-    # Look for an exact match first
     local exact_match=$(find $BAM_DIR -name "*IgG*${celltype}*${replicate}*.bam" | head -n 1)
     
     if [ -n "$exact_match" ]; then
@@ -34,7 +23,6 @@ find_igg_control() {
     fi
 }
 
-# Function to run MACS2
 run_macs2() {
     local bam_file=$1
     local output_prefix=$2
@@ -69,17 +57,12 @@ run_macs2() {
     echo "MACS2 completed for ${output_prefix}"
 }
 
-# Process all BAM files
 for bam_file in ${BAM_DIR}/*.bam; do
-    # Skip index files and IgG files
     if [[ $bam_file == *.bai ]] || [[ $bam_file == *IgG* ]]; then
         continue
     fi
     
-    # Extract the base name of the file
-    base_name=$(basename "$bam_file" .sorted.bam)
-    
-    # Find matching IgG control
+    base_name=$(basename "$bam_file" .sorted.bam)    
     control_file=$(find_igg_control "$base_name")
     
     echo "Processing file: $bam_file"
@@ -89,14 +72,12 @@ for bam_file in ${BAM_DIR}/*.bam; do
         echo "No matching control found"
     fi
     
-    # Determine the histone mark
     if [[ $base_name == *"H3K27me3"* ]]; then
         macs2_params="--broad"
     else
         macs2_params=""
     fi
     
-    # Run MACS2
     run_macs2 "$bam_file" "$base_name" "$control_file" "$macs2_params"
 done
 
